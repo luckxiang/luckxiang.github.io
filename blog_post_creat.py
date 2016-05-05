f5916f918e2b3e0395eff7c02eaa2c87c99090c2
zhangxiang <1049242886@qq.com>
2016-05-05 23:17:39 +0800

    add

--- blog_post_creat.py
+++ blog_post_creat.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3.5
 # coding=utf-8
 import time
+import os
 
-title=input("Creat new blog post\nPlease enter blog post title:")
-tags=input("Please enter tags:")
-ISOTIMEFORMAT="%Y-%m-%d"
-date=time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))
-filename="_posts/%s-%s.md" % (date,title)
+title = input('Creat new blog post\nPlease enter blog post title:')
+tags = input('Please enter tags:')
+ISOTIMEFORMAT = '%Y-%m-%d'
+date = time.strftime(ISOTIMEFORMAT, time.localtime(time.time()))
+filename = '_posts/%s-%s.md' % (date,title)
 with open(filename, 'w') as f:
-    f.write("---\n")
-    f.write("layout: post\n")
-    f.write("title: %s\n" % title)
-    f.write("date: %s\n" % date)
-    f.write("categories: blog\n")
-    f.write("tags: [%s]\n" % tags)
-    f.write("description: \n")
-    f.write("---\n\n\n")
-
+    f.write('---\n')
+    f.write('layout: post\n')
+    f.write('title: %s\n' % title)
+    f.write('date: %s\n' % date)
+    f.write('categories: blog\n')
+    f.write('tags: [%s]\n' % tags)
+    f.write('description: \n')
+    f.write('---\n\n\n')
+sys = 'vi  %s' % filename
+os.system(sys)
--- book.md
+++ book.md
@@ -9,11 +9,11 @@ header-img: "img/zhihu.jpg"
 <center>
     <p><img src="http://7xs8go.com1.z0.glb.clouddn.com/bigxiang.png" align="center"></p>
 </center>
-book 主要是我用gitbook记录的知识地图。本着融会贯通和把书读薄的精神，把一些零散的知识串联起来，汇总成书，温故知新。
+book 主要是我用gitbook记录的知识地图。本着融会贯通和把书读薄的精神，把一些零散的知识串联起来，汇总成书，温故知新，点击书籍名称即可跳转。
 
 ### 目录：
 
-1.[时雨大人的软件使用记录](/gbook/app)
+1.[《时雨大人的软件使用记录》](/gbook/app)
 
 
 
